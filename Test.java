81dc5df359b870dcc09113cdc19cc582f65db794
Ashutosh Kumar <79661344+seleniumjan2021@users.noreply.github.com>
2021-10-24 16:15:13 +0530

    learning github

--- Test.java
+++ Test.java
@@ -6,8 +6,7 @@ public class Test
 	public static void main(String args[])
 
 	{
-	   System.out.println("Hello World!");
-		System.out.println("I am adding a new line!")
+	   System.out.println("Welecome to github");
 	}
 
 
